919ce19b54aaf706fa3f6cc33a806c2fcca69bc5
Elmalem <liozelmalem@gmail.com>
2018-11-30 13:56:13 +0200

    Update
    for now,everything works well

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Navigation
+
+	hierarchy actions : File format <- GIS - > Geom ->(Coords)
+	threads <-> GUI : the main of the application
+
+	This project represents how to get data from
+	csv files and use it according to google earth,gps,and more
+	geographic data	applications.
+	every csv file transformed to kml file and 
+	the new data of kml file represents collection of 
+	GIS (Geographic Information System) element.
+	each GIS element contain some types of data 
+	for use in the GIS applications.
+	
+	for users :
+	 
+	the easy way to use in this code 
+	
+	1) understand the goal idea of this project,the theory(some lines up).
+	2) see the class diagram and understand the hierarchy actions.
+	3) go to the project functions and enjoy it.
--- src/GIS/Filewalker.java
+++ src/GIS/Filewalker.java
@@ -17,99 +17,88 @@ public class Filewalker {
 		String csvSplitBy = ",";
 		ArrayList<String[]> data = new ArrayList<>();
 		String[] dataLine;
-		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-			while ((line = br.readLine()) != null) {
-				dataLine = line.split(csvSplitBy);
-				data.add(dataLine);
+		if (csvFile.charAt(csvFile.length() - 1) == 'v') {
+			try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+				while ((line = br.readLine()) != null) {
+					dataLine = line.split(csvSplitBy);
+					data.add(dataLine);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		return data;
 	}
 
-    public ArrayList<String> walk( String path ) throws Exception {
+	public ArrayList<String> walk(String path) throws Exception {
 
-		ArrayList<String> ans = new ArrayList<String>();	
-        File root = new File( path );
-        File[] list = root.listFiles();
+		ArrayList<String> ans = new ArrayList<String>();
+		File root = new File(path);
+		File[] list = root.listFiles();
 
-        if (list == null) ans = null;
+		if (list == null)
+			ans = null;
 
-        for ( File f : list ) {
-            if ( f.isDirectory() ) {
-                walk( f.getAbsolutePath() );
-                System.out.println( "Dir:" + f.getAbsoluteFile() );
-            }
-            else {
-    			ans.add(f.toString());
-                System.out.println( "File:" + f.getAbsoluteFile() );
-            }
-        }
-    return ans;
-    }
-    public static MyGIS_project creat_project (ArrayList<String> file) throws Exception
-    {
-    	MyGIS_layer[] layers = new MyGIS_layer[file.size()];
+		for (File f : list) {
+			if (f.isDirectory()) {
+				walk(f.getAbsolutePath());
+				System.out.println("Dir:" + f.getAbsoluteFile());
+			} else {
+				ans.add(f.toString());
+				System.out.println("File:" + f.getAbsoluteFile());
+			}
+		}
+		return ans;
+	}
+
+	public static MyGIS_project creat_project(ArrayList<String> file) throws Exception {
+		MyGIS_layer[] layers = new MyGIS_layer[file.size()];
 //		MyGIS_layer p1 = null;		
-        for (int i = 0; i < file.size(); i++) {
-            ArrayList<String[]> ans =  loadCsvLine(file.get(i));
-            MyGIS_layer layer= creat_Layer (ans);
-            layers[i]= layer;
-            System.out.println("project: ");
-            System.out.println("");
+		for (int i = 0; i < file.size(); i++) {
+			ArrayList<String[]> ans = loadCsvLine(file.get(i));
+			MyGIS_layer layer = creat_Layer(ans);
+			layers[i] = layer;
+			System.out.println("project: ");
+			System.out.println("");
 		}
 		MyGIS_project pol = new MyGIS_project(layers);
 		return pol;
-    }
+	}
 
-    
-    
-    
-    public static MyGIS_layer creat_Layer (ArrayList<String[]> layer)
-    {
-    	MyGIS_Element element;
-    	ArrayList<GIS_element> ans = new ArrayList<GIS_element>();
-    	for (int i = 2; i < layer.size(); i++) {
-    		element = creat_Element(layer.get(i));
-    		System.out.println(element.toString());
-    		ans.add(element);
+	public static MyGIS_layer creat_Layer(ArrayList<String[]> layer) {
+		MyGIS_Element element;
+		ArrayList<GIS_element> ans = new ArrayList<GIS_element>();
+		for (int i = 2; i < layer.size(); i++) {
+			element = creat_Element(layer.get(i));
+			System.out.println(element.toString());
+			ans.add(element);
 		}
-		MyGIS_layer p1 = new MyGIS_layer(ans);		
+		MyGIS_layer p1 = new MyGIS_layer(ans);
 //    	System.out.println(p1.toString());
-    	return p1;
-    	
-    }
-    
-    public static MyGIS_Element creat_Element (String[] element)
-    {
-    	String full="";
-    	for (int i = 0; i < element.length-1; i++) {
-			full= full + element[i] + ",";
-		}
-    	full = full + element[element.length-1];
-//    	System.out.println(full);
-    	MyGIS_Element ans = new MyGIS_Element(full);
-    	return ans;
-    }
+		return p1;
 
-    
+	}
+
+	public static MyGIS_Element creat_Element(String[] element) {
+		String full = "";
+		for (int i = 0; i < element.length - 1; i++) {
+			full = full + element[i] + ",";
+		}
+		full = full + element[element.length - 1];
+//    	System.out.println(full);
+		MyGIS_Element ans = new MyGIS_Element(full);
+		return ans;
+	}
 
 //    public static 
-    
-    public static void main(String[] args) throws Exception {
-        Filewalker fw = new Filewalker();
-        ArrayList<String> h = fw.walk("C:\\Users\\nahama\\Desktop\\Ex2\\Ex2\\data");
-        MyGIS_project ans = creat_project(h);
-        
-        
-        System.out.println(ans.toString());
 
-        
-		}
-        
-        
-        
-    }
+	public static void main(String[] args) throws Exception {
+		Filewalker fw = new Filewalker();
+		ArrayList<String> h = fw.walk("C:\\Users\\DELL\\Desktop\\Ex2\\Ex2\\data");
+		MyGIS_project ans = creat_project(h);
 
+		System.out.println(ans.toString());
 
+	}
+
+}
